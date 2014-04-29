923ee1aaad7f3b5ebbd77d2e6db0aa2d9065ad02
navyon <net_systems@live.com>
2014-04-29 13:25:52 +0200

    added agent states

--- Arduino/agentcontrol/agentcontrol.ino
+++ Arduino/agentcontrol/agentcontrol.ino
@@ -1,7 +1,8 @@
 #define LED 9 // the pin for the LED
 #define LED10 10 // the pin for the LED
 #define LED11 11
-#include <Servo.h> 
+#include <Servo.h>
+#include <math.h>
  
 Servo myservo;              
 int count =0;
@@ -12,11 +13,17 @@ int fadeAmount = 5;    // how many points to fade the LED by
 
 
 void setup() { // bring the LED up nicely from being off
+ for(i = 0 ; i <= 15; i+=1)
+  {
+    analogWrite(LED, i);
+    delay(5);
+  }
  pinMode(LED, OUTPUT); 
  pinMode(LED10, OUTPUT); 
  pinMode(LED11, OUTPUT);
  myservo.attach(8);
-  Serial.begin(9600);
+ Serial.begin(9600);
+  
 }
 void loop()                     
 {
@@ -57,25 +64,36 @@ if(Serial.available())
  }
 }
 void State0(){
-  
+  while(!Serial.available()){
     digitalWrite(LED11,HIGH);
+  }
+  digitalWrite(LED11,LOW);
 }
 void State1(){
+  while(!Serial.available()){
   
- digitalWrite(LED10,HIGH);  
-  
+    digitalWrite(LED10,HIGH); 
+  } 
+  digitalWrite(LED10,LOW); 
 } 
 void State2(){
-  
+  while(!Serial.available()){
   digitalWrite(LED, HIGH);   // turn the LED on (HIGH is the voltage level)
   delay(1000);               // wait for a second
   digitalWrite(LED, LOW);    // turn the LED off by making the voltage LOW
-  delay(1000);   
-  
+  delay(1000); 
+  }  
+  digitalWrite(LED, LOW);
 } 
 void State3(){
   
-  
+   
+  while(!Serial.available()){
+  float val = (exp(sin(millis()/2000.0*PI)) - 0.36787944)*108.0;
+  analogWrite(LED11, val); 
+
+  }
+    analogWrite(LED11, LOW); 
   
 } 
 void State4(){
